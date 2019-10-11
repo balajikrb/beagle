4a3fa41ab6498caabb54bdb6f45124bfa4fd30d2
Markus von Rüden <mvr@opennms.com>
2019-10-12 00:30:27 +0200

    ISSUE-49: Add more logging before acteually running the tests

--- .circleci/e2e.sh
+++ .circleci/e2e.sh
@@ -5,6 +5,7 @@ echo "yarn" $(yarn --version)
 echo "npm" $(npm --version)
 echo "protractor" $(protractor --version)
 google-chrome --version
+webdriver-manager version
 java -version
 curl --version
 
@@ -15,6 +16,7 @@ yarn
 echo "Starting selenium server"
 webdriver-manager update
 webdriver-manager start &
+webdriver-manager status
 
 echo "Running UI tests"
 protractor --troubleshoot true --baseUrl='http://localhost:8080' src/test/javascript/conf.js || exit 1
